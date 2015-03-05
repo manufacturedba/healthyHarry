a92e168c8ba62afa66bc8298725b6a5619365a00
manufacturedba <manufacturedba@gmail.com>
2015-03-04 22:01:39 -0600

    Setup game inside Window prop.

--- src/healthyharry.js
+++ src/healthyharry.js
@@ -1,1 +1,18 @@
-// DAS HEALTHY HARRY VIDEO GAME
+var Game = function(height, width, element){
+
+    var game = new Phaser.Game(height, width, Phaser.AUTO, element | '', {
+        preload: preload,
+        create: create
+    });
+
+    function preload(){
+        // pass
+    }
+
+    function create(){
+        // pass
+    }
+};
+
+// Is there a better way of doing this?
+window.Game = Game;
