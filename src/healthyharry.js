1020d17563611ad8400c0c162eada4e8fc703b25
manufacturedba <manufacturedba@gmail.com>
2015-03-05 22:33:10 -0600

    Simple demo of moving sprite across screen.

--- src/healthyharry.js
+++ src/healthyharry.js
@@ -1,15 +1,28 @@
 var Game = function(height, width, element){
 
-    var game = new Phaser.Game(height, width, Phaser.AUTO, element | '', {
+    var game = new Phaser.Game(width, height, Phaser.AUTO, element || '', {
         preload: preload,
-        create: create
+        create: create,
+        update: update
     });
 
+    var cursors, sprite;
+
     function preload(){
-        // pass
+        game.load.atlasJSONHash('atlas', 'static/spritesheet.png', 'static/sprites.json');
     }
 
     function create(){
-        // pass
+        sprite = game.add.sprite(0, height - 50, 'atlas', 'man1');
+        game.physics.enable(sprite, Phaser.Physics.ARCADE);
+        sprite.height = 50;
+        sprite.width = 50;
+        cursors = game.input.keyboard.createCursorKeys();
+    }
+
+    function update(){
+        sprite.body.velocity.setTo(0, 0);
+        if(cursors.left.isDown) sprite.body.velocity.x = -50;
+        if(cursors.right.isDown) sprite.body.velocity.x = 50;
     }
 };
